8e0de8cccbc74c920fb20d4cfad9424acd6394bc
Mariana Tavares <70786454+maritavares@users.noreply.github.com>
2020-09-07 14:45:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+// questões do quizz 
+var myQuestions = [
+	{
+		question: "",
+		answers: {
+			a: '',
+			b: '',
+			c: ''
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "?",
+		answers: {
+			a: '',
+			b: '',
+			c: '',
+			d: ''
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "",
+		answers: {
+			a: '',
+			b: '',
+			c: '',
+			d: ''
+		},
+		correctAnswer: 'a'
+	}
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+// gera a resposta na tela 
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		
+		var output = [];
+		var answers; 
+
+		
+		for(var i=0; i<questions.length; i++){
+			
+			
+			answers = [];
+
+			
+			for(letter in questions[i].answers){
+
+				
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+			
+		
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		
+		
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		
+		for(var i=0; i<questions.length; i++){
+
+			
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			
+			
+			
+			if(userAnswer===questions[i].correctAnswer){
+				
+				numCorrect++;
+				
+				
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			
+			else{
+				
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+			
+			resultsContainer.innerHTML = numCorrect + ' de ' + questions.length;
+		}
+
+	
+	showQuestions(questions, quizContainer);
+
+	
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
